97c4260f5dae41a2351322a80990041ba120439b
Kleinrad <50015410+Kleinrad@users.noreply.github.com>
2021-06-14 21:47:52 +0200

    removed test

--- app.js
+++ app.js
@@ -221,10 +221,6 @@ io.on("connection", (socket) => {
     );
     let role = connectedUsers[absClientId].role;
     let now = new Date();
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        socket.emit("task", 3);
-        return;
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     if (interaction != undefined) {
       if (
         interaction == -2 ||
